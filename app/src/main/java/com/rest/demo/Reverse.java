5640225cb1ca1ad5627693bbf685e65b257a93eb
Tommy Tho <tommythowm@gmail.com>
2020-05-06 21:54:53 -0700

    implemented string reversal in model

--- app/src/main/java/com/rest/demo/Reverse.java
+++ app/src/main/java/com/rest/demo/Reverse.java
@@ -8,8 +8,16 @@ public class Reverse {
         this.string = getContent(string);
     }
     
-    public String getContent(String string) {
-        // TODO: Reverse the string then return it
+    public String getContent(String str) {
+
+        char[] oldString = str.toCharArray();
+        char[] newString = new char[str.length()];
+
+        for(int i = 0; i < str.length(); i++) {
+            newString[i] = oldString[str.length() - i];
+        }
+
+        string = String.valueOf(newString);
 
         return string;
     }
