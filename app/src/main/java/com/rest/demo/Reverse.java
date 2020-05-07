f0c0f87a1487ddde92d81b589cfc338c4127019c
Tommy Tho <tommythowm@gmail.com>
2020-05-06 21:32:18 -0700

    added model that takes strings

---
+++ app/src/main/java/com/rest/demo/Reverse.java
@@ -0,0 +1,16 @@
+package com.rest.demo;
+
+public class Reverse {
+    
+    private String string = "";
+
+    public Reverse(String string) {
+        this.string = string;
+    }
+    
+    public String getContent(String string) {
+        // TODO: Reverse the string then return it
+
+        return string;
+    }
+}
