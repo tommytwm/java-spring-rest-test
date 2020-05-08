9750ac12cb45802dafd302e9ee561682dac93111
Tommy Tho <tommythowm@gmail.com>
2020-05-07 23:00:06 -0700

    fixed white label page error for empty uri

--- app/src/main/java/com/rest/demo/DemoApplication.java
+++ app/src/main/java/com/rest/demo/DemoApplication.java
@@ -1,11 +1,12 @@
 package com.rest.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
